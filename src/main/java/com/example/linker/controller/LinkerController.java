42c20941ef5ba9a649b361e61b298951f50acf31
applepinepaprica <applepinepaprica@gmail.com>
2018-06-26 23:09:43 +0500

    navbar

--- src/main/java/com/example/linker/controller/LinkerController.java
+++ src/main/java/com/example/linker/controller/LinkerController.java
@@ -1,7 +1,13 @@
 package com.example.linker.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -75,5 +81,14 @@ public class LinkerController {
 			@PathVariable String fileName,
 			Model model) {
 		return noteService.getFileDataByUrl(url, fileName);
+	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logout (HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "redirect:/login?logout";
 	}
 }
