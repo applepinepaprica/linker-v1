d47240f6efccdfeb2ee04769bbbb170fd9665c0f
applepinepaprica <applepinepaprica@gmail.com>
2018-10-11 11:13:56 +0300

    Pfff

--- src/main/java/com/example/linker/LinkerApplication.java
+++ src/main/java/com/example/linker/LinkerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LinkerApplication {
 
-//Comment
+//PFFFFFF
 	public static void main(String[] args) {
 		SpringApplication.run(LinkerApplication.class, args);
 	}
