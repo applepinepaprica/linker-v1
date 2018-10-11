9b77fc84105be3d547f2837e652581d29c6f9739
applepinepaprica <applepinepaprica@gmail.com>
2018-10-11 11:17:30 +0300

    Conflict

--- src/main/java/com/example/linker/LinkerApplication.java
+++ src/main/java/com/example/linker/LinkerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LinkerApplication {
 
-//PFFFFFF
+//trying to create conflict
 	public static void main(String[] args) {
 		SpringApplication.run(LinkerApplication.class, args);
 	}
