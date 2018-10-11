d0a0b219882ea460580720842a22faccc947706e
applepinepaprica <applepinepaprica@gmail.com>
2018-10-11 11:18:22 +0300

    Conflict master

--- src/main/java/com/example/linker/LinkerApplication.java
+++ src/main/java/com/example/linker/LinkerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LinkerApplication {
 
+//With two branches
 	public static void main(String[] args) {
 		SpringApplication.run(LinkerApplication.class, args);
 	}
